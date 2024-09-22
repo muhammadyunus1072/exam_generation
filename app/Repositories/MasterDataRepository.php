5a1938c8774eb9c50e129e0750c97a9c6507bc10
Bhagaskara <bhagaskaraliancer@gmail.com>
2024-09-22 20:01:46 +0700

    Update Master Data Repository

--- app/Repositories/MasterDataRepository.php
+++ app/Repositories/MasterDataRepository.php
@@ -6,29 +6,48 @@ abstract class MasterDataRepository
 {
     abstract protected static function className(): string;
 
-    public static function find($id)
-    {
-        return app(static::className())->find($id);
-    }
-
     public static function create($data)
     {
         return app(static::className())::create($data);
     }
 
+    public static function find($id)
+    {
+        return app(static::className())->find($id);
+    }
+
+    public static function findBy($whereClause)
+    {
+        $query = app(static::className())->query();
+        foreach ($whereClause as $clause) {
+            $query->where($clause['column'], $clause['operator'], $clause['value']);
+        }
+
+        return $query->first();
+    }
+
     public static function update($id, $data)
     {
         $obj = self::find($id);
-        return $obj->update($data);
+        return empty($obj) ? false : $obj->update($data);
+    }
+
+    public static function updateBy($whereClause, $data)
+    {
+        $obj = self::findBy($whereClause);
+        return empty($obj) ? false : $obj->update($data);
     }
 
     public static function delete($id)
     {
         $obj = self::find($id);
-        if ($obj) {
-            return $obj->delete();
-        }
-        return false;
+        return empty($obj) ? false : $obj->delete();
+    }
+
+    public static function deleteBy($whereClause)
+    {
+        $obj = self::findBy($whereClause);
+        return empty($obj) ? false : $obj->delete();
     }
 
     public static function all()
