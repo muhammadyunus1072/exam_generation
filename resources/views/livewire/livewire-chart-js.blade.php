3c3abd42bd30134dad37fa5ef18921600917c48f
Bhagaskara <bhagaskaraliancer@gmail.com>
2024-10-03 15:03:05 +0700

    Add Content Before After Chart Js

--- resources/views/livewire/livewire-chart-js.blade.php
+++ resources/views/livewire/livewire-chart-js.blade.php
@@ -1,16 +1,18 @@
 <div class="{!! $containerClass !!}" style="{!! $containerStyle !!}" wire:ignore>
+    @yield('content-before')
     <canvas id="{{ $canvasId }}"></canvas>
+    @yield('content-after')
 </div>
 
-@script
+@push('js')
     <script>
         const config = @json($config);
         const chart = new Chart(document.getElementById('{{ $canvasId }}'), config);
 
-        $wire.on('js-chart-update', (data) => {
+        Livewire.on('js-chart-update', (data) => {
             chart.data.datasets = data.datasets;
             chart.data.labels = data.labels;
             chart.update();
         });
     </script>
-@endscript
+@endpush
